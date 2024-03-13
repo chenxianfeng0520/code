6043def5d43c618646f1beda428c87de12833c4e
cancel <chenxianfeng0520@gmail.com>
2024-03-13 10:24:33 +0800

    2222

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-// import Antd from 'ant-design-vue';
-// import 'ant-design-vue/dist/reset.css';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import router from "./router/index.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import registerComponent from "@/components/index.js";
 
-import tailwindcss from 'tailwindcss'
+// import tailwindcss from 'tailwindcss'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
@@ -19,7 +19,7 @@ const app = createApp(App);
 registerComponent(app);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
-app.use(tailwindcss);
+// app.use(tailwindcss);
 
-// app.use(Antd)
+app.use(Antd)
 app.mount("#app");
