a906e1cb871e6886b6e2807c92c712da8426bf06
cancel <chenxianfeng0520@gmail.com>
2024-03-16 11:59:40 +0800

    2222222

--- src/views/index.vue
+++ src/views/index.vue
@@ -13,11 +13,11 @@ function toPage(pagekey) {
       path: "/Dockerfile",
     });
   }
-  if (pagekey == 3) {
-    router.push({
-      path: "/MonacoEditor",
-    });
-  }
+  // if (pagekey == 3) {
+  //   router.push({
+  //     path: "/MonacoEditor",
+  //   });
+  // }
 }
 </script>
 <template>
@@ -41,13 +41,13 @@ function toPage(pagekey) {
     >
       <span>基于vue-codemirror生成Dockerfile编辑器</span>
     </a-card>
-    <a-card
+    <!-- <a-card
       :bordered="false"
       class="Docker animate__animated animate__bounceIn"
       @click="toPage(3)"
     >
       <span>基于monaco-editor生成Dockerfile编辑器</span>
-    </a-card>
+    </a-card> -->
   </div>
 </template>
 
