7d524f44950a7ea92e6c3b92e116f112def7f0e2
cancel <chenxianfeng0520@gmail.com>
2024-03-29 17:59:09 +0800

    4444

--- src/views/Minio.vue
+++ src/views/Minio.vue
@@ -2,15 +2,44 @@
 import { getList } from "@/api/lottery.js";
 const list = ref([]);
 getList().then((res) => {
-  console.log(res.data.data, "res.data");
   list.value = res.data.data;
 });
 </script>
 <template>
-  <div class="card" style="width: 18rem" v-for="item in list">
-    <div class="card-body">
-      <h5 class="card-title">{{ item.name || item.prefix }}</h5>
-    </div>
+  <div class="main-page">
+    <a-card
+      :bordered="false"
+      class="minio animate__animated animate__bounceIn"
+      v-for="item in list"
+      >{{ item.name || item.prefix }}</a-card
+    >
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main-page {
+  padding: 120px 160px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, calc(25% - 70px));
+  grid-column-gap: 70px;
+  cursor: pointer;
+  .ant-card {
+    font-size: 16px;
+    line-height: 22px;
+    color: #cedeff;
+    border-radius: 25px;
+    height: 90px;
+    font-family: sans-serif;
+    margin-bottom: 60px;
+    &:hover {
+      text-decoration: underline;
+    }
+    :deep(.ant-card-body) {
+      padding: 20px 15px 0 70px;
+    }
+  }
+  .minio {
+    background: 15px 25px / 40px 40px no-repeat url(@/assets/files.png),
+      #ffffff1a;
+  }
+}
+</style>
